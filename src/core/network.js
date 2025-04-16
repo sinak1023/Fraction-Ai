6aae9762faad40e7e591f32549d50a8512fbf22c
sinox0101 <129672206+sinak1023@users.noreply.github.com>
2025-04-17 00:09:37 +0330

    Create network.js

---
+++ src/core/network.js
@@ -0,0 +1,8 @@
+class Network {
+  static CHAINID = 11155111;
+  static RPCURL = "https://ethereum-sepolia-rpc.publicnode.com";
+  static EXPLORER = "https://sepolia.etherscan.io/";
+  static SYMBOL = "ETH";
+}
+
+export default Network;
